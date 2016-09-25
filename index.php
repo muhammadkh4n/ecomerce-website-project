5609a8d34ed65089fa4d8c5de56ce5027eb98dab
Muhammad Khan <muhammadkh4n@gmail.com>
2016-09-25 16:17:57 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include_once "template/head.php"; ?>
+
+<div class="index">
+
+    <?php
+
+    include_once "template/header.php";
+    include_once "template/store-front.php";
+    include_once "template/footer.php";
+
+    ?>
+
+</div>
+
+</body>
+</html>
---
+++ template/store-front.php
@@ -0,0 +1,344 @@
+<main>
+
+    <div class="main-navigation">
+        <div class="container">
+            <section class="categories">
+                <ul class="category-list">
+                    <li>
+                        <a href="#">Phones</a>
+                        <?php //include_once("drop-menu.php"); ?>
+                    </li>
+                    <li>
+                        <a href="#">Phone Accessories</a>
+                    </li>
+                    <li>
+                        <a href="#">Chargers & Docks</a>
+                    </li>
+                    <li>
+                        <a href="#">Computers</a>
+                    </li>
+                    <li>
+                        <a href="#">Laptops</a>
+                    </li>
+                    <li>
+                        <a href="#">Tablets</a>
+                    </li>
+                </ul>
+            </section>
+        </div>
+    </div>
+
+    <div class="container">
+        <section class="featured">
+            <div class="slider">
+                <a href="#" class="slide-change prev" id="prev"><span></span></a>
+                <a href="#" class="slide-change next" id="next"><span></span></a>
+                <ul class="slides">
+                    <li class="slide"><a href="#slide1"><img src="img/slide1.jpg" alt="Slide 1"/></a></li>
+                    <li class="slide"><a href="#slide2"><img src="img/slide2.jpg" alt="Slide 2"/></a></li>
+                    <li class="slide"><a href="#slide3"><img src="img/slide3.jpg" alt="Slide 3"/></a></li>
+                    <li class="slide"><a href="#slide4"><img src="img/slide4.jpg" alt="Slide 4"/></a></li>
+                    <li class="slide"><a href="#slide5"><img src="img/slide5.jpg" alt="Slide 5"/></a></li>
+                    <li class="slide"><a href="#slide6"><img src="img/slide6.jpg" alt="Slide 6"/></a></li>
+                    <li class="slide"><a href="#slide1"><img src="img/slide1.jpg" alt="Slide 1"/></a></li>
+                </ul>
+                <ul class="slider-nav">
+                    <li class="current"></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </section>
+        <section class="featured-items">
+            <div class="items">
+                <ul>
+                    <li class="item">
+                        <a href="#">
+                            <img src="img/gs6e.jpg" alt="Samsung Galaxy S6 Edge"/>
+                            <span class="img-label">
+                                <p>
+                                    Samsung Galaxy S6 Edge<br>
+                                    <i>$699.0</i>
+                                </p>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="item">
+                        <a href="#">
+                            <img src="img/ambp.jpg" alt="Apple Macbook Pro"/>
+                            <span class="img-label">
+                                <p>
+                                    Apple Macbook Pro<br>
+                                    <i>$1299.0</i>
+                                </p>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="item">
+                        <a href="#">
+                            <img src="img/alien.jpg" alt="Dell Alienware" class="big"/>
+                            <span class="img-label">
+                                <p>
+                                    Dell Alienware<br>
+                                    <i>$2799.0</i>
+                                </p>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <div class="container">
+        <section class="today-deals product-section">
+            <header>
+                <h1>Today's Deals</h1>
+                <div class="tabs">
+                    <ul id="today-tabs">
+                        <li class="tab active" id="phone-tab"><a href="#">Phones</a></li>
+                        <li class="tab" id="computer-tab"><a href="#">Computers</a></li>
+                    </ul>
+                </div>
+            </header>
+
+            <div class="clearfix"></div>
+
+            <div class="today-items">
+                <div class="category-items phone-items active">
+                    <ul class="left-items">
+                        <li class="item">
+                            <a href="#">
+                                <img src="img/today-g4.jpg" alt="LG G4 Hot Deal"/>
+                                <h2>LG G4</h2>
+                                <span class="price-old">$779.0</span><i class="price">$559</i><br>
+                                <i class="discount">-30%</i>
+                            </a>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <img src="img/today-z4.jpg" alt="Sony Xperia Z4 Hot Deal"/>
+                                <h2>Sony Xperia Z4</h2>
+                                <span class="price-old">$599.0</span><i class="price">$559</i><br>
+                                <i class="discount">-40%</i>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="right-items">
+                        <li class="item">
+                            <a href="#">
+                                <div class="img-box">
+                                    <img src="img/today-passport.jpg" alt="Blackberry Passport"/>
+                                </div>
+                                <h2>Blackberry Passport</h2>
+                                <span class="price-old">$559.0</span><i class="price">$559</i><br>
+                                <i class="discount">-25%</i>
+                            </a>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <div class="img-box">
+                                    <img src="img/today-ip6.jpg" alt="Apple IPhone 6"/>
+                                </div>
+                                <h2>Apple IPhone 6</h2>
+                                <span class="price-old">$749.0</span><i class="price">$559</i><br>
+                                <i class="discount">-15%</i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="category-items computer-items">
+                    <ul class="left-items">
+                        <li class="item">
+                            <a href="#">
+                                <img src="img/today-g4.jpg" alt="LG G4 Hot Deal"/>
+                                <h2>Dell XPS</h2>
+                                <span class="price-old">$1579.0</span><i class="price">$559</i><br>
+                                <i class="discount">-20%</i>
+                            </a>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <img src="img/today-z4.jpg" alt="Sony Xperia Z4 Hot Deal"/>
+                                <h2>Apple Macbook Pro</h2>
+                                <span class="price-old">$2599.0</span><i class="price">$559</i><br>
+                                <i class="discount">-10%</i>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="right-items">
+                        <li class="item">
+                            <a href="#">
+                                <img src="img/today-passport.jpg" alt="Blackberry Passport"/>
+                                <h2>Lenovo Yoga 3</h2>
+                                <span class="price-old">$1799.0</span><i class="price">$559</i><br>
+                                <i class="discount">-25%</i>
+                            </a>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <img src="img/today-ip6.jpg" alt="Apple IPhone 6"/>
+                                <h2>Asus Zenbook Elite</h2>
+                                <span class="price-old">$2349.0</span><i class="price">$559</i><br>
+                                <i class="discount">-15%</i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        <section class="phones product-section">
+            <header>
+                <h1>Phones and Accessories</h1>
+            </header>
+
+            <div class="phone-box">
+                <div class="phone-items">
+                    <ul>
+                        <li class="item">
+                            <a href="#">
+                                <h2>Samsung Galaxy Note 5</h2>
+                                <i class="category">Smartphone</i>
+                                <div class="img-box">
+                                    <img src="img/phone-note5.jpg" alt="Samsung Galaxy Note 5"/>
+                                </div>
+                            </a>
+                            <span><a href="#" class="price">$749.0</a></span>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <h2>One Plus 2</h2>
+                                <i class="category">Smartphone</i>
+                                <div class="img-box">
+                                    <img src="img/phone-oneplus2.jpg" alt="One Plus Two"/>
+                                </div>
+                            </a>
+                            <span><a href="#" class="price">$749.0</a></span>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <h2>Nillkin Wireless Charger</h2>
+                                <i class="category">Chargers and Docks</i>
+                                <div class="img-box">
+                                    <img src="img/phone-wirelesscharger.jpg" alt="Nillkin Wireless Charger"/>
+                                </div>
+                            </a>
+                            <span><a href="#" class="price">$749.0</a></span>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <h2>Nillkin Galaxy S6 Case</h2>
+                                <i class="category">Cases</i>
+                                <div class="img-box">
+                                    <img src="img/phone-nillkin.jpg" alt="Nillkin Phone Case"/>
+                                </div>
+                            </a>
+                            <span><a href="#" class="price">$749.0</a></span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="phone-slider section-slider">
+                    <a href="#" class="slide-change" id="prev"><span class="arrow"></span></a>
+                    <a href="#" class="slide-change" id="next"><span class="arrow"></span></a>
+                    <ul class="slides">
+                        <li class="slide"><a href="#slide1"><img src="img/slide-phone-g3.jpg" alt="LG G3"/></a></li>
+                        <li class="slide"><a href="#slide2"><img src="img/slide-phone-htc.jpg" alt="HTC HD2"/></a></li>
+                        <li class="slide"><a href="#slide3"><img src="img/slide-phone-s6.jpg" alt="Samsung Galaxy S6"/></a></li>
+                        <li class="slide"><a href="#slide1"><img src="img/slide-phone-g3.jpg" alt="LG G3"/></a></li>
+                    </ul>
+                    <ul class="nav-dots">
+                        <li class="current"></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        <div class="clearfix"></div>
+
+        <section class="computers product-section">
+            <header>
+                <h1>Computers and Laptops</h1>
+            </header>
+
+            <div class="computer-box">
+                <div class="computer-slider section-slider">
+                    <a href="#" class="slide-change" id="prev"><span class="arrow"></span></a>
+                    <a href="#" class="slide-change" id="next"><span class="arrow"></span></a>
+                    <ul class="slides">
+                        <li class="slide"><a href="#slide1"><img src="img/slide-phone-s6.jpg" alt="Samsung Galaxy S6"/></a></li>
+                        <li class="slide"><a href="#slide2"><img src="img/slide-phone-g3.jpg" alt="Slide 1"/></a></li>
+                        <li class="slide"><a href="#slide3"><img src="img/slide-phone-htc.jpg" alt="Slide 1"/></a></li>
+                        <li class="slide"><a href="#slide1"><img src="img/slide-phone-s6.jpg" alt="Samsung Galaxy S6"/></a></li>
+                    </ul>
+                    <ul class="nav-dots">
+                        <li class="current"></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class="computer-items">
+                    <ul>
+                        <li class="computer-hot item">
+                            <a href="#">
+                                <div class="img-box">
+                                    <img src="img/pc-item-hot.jpg" alt="Samsung Galaxy Note 5"/>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <h2>Samsung Galaxy Note 5</h2>
+                                <i class="category">Smartphone</i>
+                                <div class="img-box">
+                                    <img src="img/phone-note5.jpg" alt="Samsung Galaxy Note 5"/>
+                                </div>
+                            </a>
+                            <span><a href="#" class="price">$749.0</a></span>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <h2>One Plus 2</h2>
+                                <i class="category">Smartphone</i>
+                                <div class="img-box">
+                                    <img src="img/phone-oneplus2.jpg" alt="One Plus Two"/>
+                                </div>
+                            </a>
+                            <span><a href="#" class="price">$749.0</a></span>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <h2>Nillkin Wireless Charger</h2>
+                                <i class="category">Chargers and Docks</i>
+                                <div class="img-box">
+                                    <img src="img/phone-wirelesscharger.jpg" alt="Nillkin Wireless Charger"/>
+                                </div>
+                            </a>
+                            <span><a href="#" class="price">$749.0</a></span>
+                        </li>
+                        <li class="item">
+                            <a href="#">
+                                <h2>Nillkin Galaxy S6 Case</h2>
+                                <i class="category">Cases</i>
+                                <div class="img-box">
+                                    <img src="img/phone-nillkin.jpg" alt="Nillkin Phone Case"/>
+                                </div>
+                            </a>
+                            <span><a href="#" class="price">$749.0</a></span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </div>
+
+</main>
+
+<div class="clearfix"></div>
